b75646af6729db466ccfa1d944bb046cedee9b3a
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-05-03 02:45:49 +0000

    Revert commit

--- tokenizer-routine.sh
+++ tokenizer-routine.sh
@@ -2,7 +2,7 @@
 
 # generate mecab tokenized corpus
 python scripts/mecab_tokenization.py \
-    --input_corpus dataset/modoo-translation/merged_sentences.txt \
+    --input_corpus dataset/modoo-translation/ko_sentences.txt \
     --output_dir dataset/modoo-translation/mecab_tokenized \
     --dicdir /home/n5/chanwoo/utils/mecab-ko/lib/mecab/dic/mecab-ko-dic
 
@@ -10,3 +10,4 @@ python scripts/mecab_tokenization.py \
 python scripts/build_jamo_vocab.py \
     --vocab=200 \
     --input_corpus dataset/modoo-translation/ko_sentences.txt
+
