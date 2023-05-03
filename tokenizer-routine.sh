a6fc3111e898deb964b6acb6fd7f7681078cb7e2
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-05-03 02:36:14 +0000

    Change corpus to merged corpus

--- tokenizer-routine.sh
+++ tokenizer-routine.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# generate mecab tokenized corpus
-# python scripts/mecab_tokenization.py \
-#     --input_corpus dataset/modoo-translation/ko_sentences.txt \
-#     --output_dir dataset/modoo-translation/mecab_tokenized \
-#     --dicdir /home/n5/chanwoo/utils/mecab-ko/lib/mecab/dic/mecab-ko-dic
+generate mecab tokenized corpus
+python scripts/mecab_tokenization.py \
+    --input_corpus dataset/modoo-translation/merged_sentences.txt \
+    --output_dir dataset/modoo-translation/mecab_tokenized \
+    --dicdir /home/n5/chanwoo/utils/mecab-ko/lib/mecab/dic/mecab-ko-dic
 
 # train jamo tokenizers
 python scripts/build_jamo_vocab.py \
     --vocab=200 \
     --input_corpus dataset/modoo-translation/ko_sentences.txt
-
