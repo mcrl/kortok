ffe4200ac78463e8f3d263c3e0e8a18e1df88c3b
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-05-03 03:05:26 +0000

    Make routines run asynchronously

--- tokenizer-routine.sh
+++ tokenizer-routine.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+dict="/home/n5/chanwoo/utils/mecab-ko/lib/mecab/dic/mecab-ko-dic"
+ko_corpus="dataset/modoo-translation/ko_sentences.txt"
+en_corpus="dataset/modoo-translation/en_sentences.txt"
+merged_corpus="dataset/modoo-translation/merged_sentences.txt"
+
+# train word tokenizers
+python scripts/build_word_vocab.py \
+    --vocab=64000 \
+    --input_corpus $ko_corpus &
+
 # generate mecab tokenized corpus
 python scripts/mecab_tokenization.py \
-    --input_corpus dataset/modoo-translation/ko_sentences.txt \
+    --input_corpus $ko_corpus \
     --output_dir dataset/modoo-translation/mecab_tokenized \
-    --dicdir /home/n5/chanwoo/utils/mecab-ko/lib/mecab/dic/mecab-ko-dic
+    --dicdir $dict
 
 # train jamo tokenizers
 python scripts/build_jamo_vocab.py \
     --vocab=200 \
-    --input_corpus dataset/modoo-translation/ko_sentences.txt
+    --input_corpus $ko_corpus &
 
 # train char tokenizers
 python scripts/build_char_vocab.py \
     --vocab=2000 \
-    --input_corpus dataset/modoo-translation/ko_sentences.txt
+    --input_corpus $ko_corpus &
+
+wait
