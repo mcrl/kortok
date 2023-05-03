1b889282f9579231586d9c2a184e9af0a3f716af
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-05-03 02:26:47 +0000

    Add mecab tokenization process to the routine

--- scripts/mecab_tokenization.py
+++ scripts/mecab_tokenization.py
@@ -11,15 +11,15 @@ import MeCab
 
 INPUT_CORPUS = "dataset/modoo-translation/ko_sentences.txt"
 OUTPUT_DIR = "./dataset/modoo-translation/mecab_tokenized"
-
-TOKENIZER = MeCab.Tagger(f"-r/dev/null --dicdir /home/n5/chanwoo/utils/mecab-ko/lib/mecab/dic/mecab-ko-dic")
+DEFAULT_DICT = "/usr/local/lib/mecab/dic/mecab-ko-dic"
+global_tokenizer = None  # type: Optional[MeCab.Tagger]
 
 
 def tokenize(text: str, space_symbol: str = "▃") -> List[str]:
     text = text.strip()
     text_ptr = 0
     tokenized = []
-    for mor in TOKENIZER.parse(text).split("\n"):
+    for mor in global_tokenizer.parse(text).split("\n"):
         if "\t" in mor:
             splitted = mor.split("\t")
             token = splitted[0]
@@ -42,30 +42,34 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--space_symbol", type=str, default="▃")
     parser.add_argument("--n_jobs", type=int, default=mp.cpu_count())
+    parser.add_argument("--dicdir", type=str, default=DEFAULT_DICT, help="mecab dictionary directory")
+    parser.add_argument("--input_corpus", type=str, default=INPUT_CORPUS, help="input corpus path")
+    parser.add_argument("--output_dir", type=str, default=OUTPUT_DIR, help="output directory")
     args = vars(parser.parse_args())
     print(args)
 
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    os.makedirs(args["output_dir"], exist_ok=True)
+    global_tokenizer = MeCab.Tagger(f"-r/dev/null --dicdir {args['dicdir']}")
 
     # set tokenizing func
     tokenize_fn = partial(tokenize, space_symbol=args["space_symbol"])
 
     start_time = time.time()
     print(f"start tokenization ...")
-    with open(INPUT_CORPUS, "r", encoding="utf-8") as f:
+    with open(args["input_corpus"], "r", encoding="utf-8") as f:
         with Pool(args["n_jobs"]) as p:
             tokenized = p.map(tokenize_fn, f)
     elapsed_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
     print(f"complete tokenization for all files. (elapsed time: {elapsed_time})")
 
     # mecab tokenized corpus
-    with open(os.path.join(OUTPUT_DIR, os.path.basename(INPUT_CORPUS)), "w", encoding="utf-8") as f:
+    with open(os.path.join(args["output_dir"], os.path.basename(args["input_corpus"])), "w", encoding="utf-8") as f:
         for tokens in tokenized:
             f.write(" ".join(tokens) + "\n")
 
     # mecab config
     print("write mecab config file...")
-    output_config_path = os.path.join(OUTPUT_DIR, "tok.json")
+    output_config_path = os.path.join(args["output_dir"], "tok.json")
     with open(output_config_path, "w", encoding="utf-8") as f:
         json.dump(args, f, indent=4)
 
