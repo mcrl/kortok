7b22922d7e83f463ee0ffc2a8912da856281db88
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-05-03 02:32:59 +0000

    Add jamo tokenizer step

--- tokenizer-routine.sh
+++ tokenizer-routine.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # generate mecab tokenized corpus
-python scripts/mecab_tokenization.py \
-    --input_corpus dataset/modoo-translation/ko_sentences.txt \
-    --output_dir dataset/modoo-translation/mecab_tokenized \
-    --dicdir /home/n5/chanwoo/utils/mecab-ko/lib/mecab/dic/mecab-ko-dic
+# python scripts/mecab_tokenization.py \
+#     --input_corpus dataset/modoo-translation/ko_sentences.txt \
+#     --output_dir dataset/modoo-translation/mecab_tokenized \
+#     --dicdir /home/n5/chanwoo/utils/mecab-ko/lib/mecab/dic/mecab-ko-dic
 
-# train char tokenizers
+# train jamo tokenizers
+python scripts/build_jamo_vocab.py \
+    --vocab=200 \
+    --input_corpus dataset/modoo-translation/ko_sentences.txt
 
