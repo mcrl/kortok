db9929f243081f1dbc71bbfa571caf783075a42b
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-05-07 09:24:09 +0000

    Sequential preprocessing routines

--- generate_tokenized_sentences.py
+++ generate_tokenized_sentences.py
@@ -3,9 +3,19 @@ from pathlib import Path
 from tqdm import tqdm
 from functools import partial
 import os
-import sys
+from multiprocessing import Process
 
 DICT_PATH = "/home/n5/chanwoo/utils/mecab-ko/lib/mecab/dic/mecab-ko-dic"
+KO_CORPUSES = [
+    Path("dataset/modoo-translation/processed/train.ko"),
+    Path("dataset/modoo-translation/processed/validation.ko"),
+    Path("dataset/modoo-translation/processed/test.ko"),
+]
+EN_CORPUSES = [
+    Path("dataset/modoo-translation/processed/train.en"),
+    Path("dataset/modoo-translation/processed/validation.en"),
+    Path("dataset/modoo-translation/processed/test.en"),
+]
 
 
 def tokenize_sentences(input_file, output_file, tokenizer: BaseTokenizer, pbar=False):
@@ -16,96 +26,98 @@ def tokenize_sentences(input_file, output_file, tokenizer: BaseTokenizer, pbar=F
     with open(input_file, "r") as f:
         lines = f.readlines()
 
-    print("Tokenizing...")
-    print(f"Input file: {input_file}")
-    print(f"Output file: {output_file}")
-
     with open(output_file, "w") as f:
         for line in tqdm(lines) if pbar else lines:
             tokenized = tokenizer.tokenize(line)
             tokenized = " ".join(tokenized)
             f.write(tokenized + "\n")
-        print("Saving File...... ", end="")
-        sys.stdout.flush()
-
-    print("Done!")
-    print("=" * 30)
 
 
 if __name__ == "__main__":
-    input_file = "dataset/modoo-translation/ko_sentences.txt"
+    processes = []
     resources = os.listdir("resources")
 
-    tasks = []
-    # create char tokenizer task
-    output_file = "dataset/modoo-translation/tokenized/ko_sentences_char-2k.txt"
     tokenizer = CharTokenizer()
-    task = partial(tokenize_sentences, input_file, output_file, tokenizer)
-    print(f"char_tokenizer")
-    tasks.append(task)
+    for input_file in KO_CORPUSES:
+        output_file = f"dataset/modoo-translation/tokenized/char-2k/{input_file.name}"
+        task = partial(tokenize_sentences, input_file, output_file, tokenizer)
+        process = Process(target=task)
+        process.start()
+        processes.append(process)
+        print(f"CharTokenizer: {input_file.name} is processing...")
 
     # JamoTokenizer
-    output_file = "dataset/modoo-translation/tokenized/ko_sentences_jamo-200.txt"
     tokenizer = JamoTokenizer()
-    task = partial(tokenize_sentences, input_file, output_file, tokenizer)
-    print(f"jamo_tokenizer")
-    tasks.append(task)
-
-    # WordTokenizer
-    output_file = "dataset/modoo-translation/tokenized/ko_sentences_word-64k.txt"
-    tokenizer = WordTokenizer()
-    task = partial(tokenize_sentences, input_file, output_file, tokenizer)
-    print(f"word_tokenizer")
-    tasks.append(task)
+    for input_file in KO_CORPUSES:
+        output_file = f"dataset/modoo-translation/tokenized/jamo-200/{input_file.name}"
+        task = partial(tokenize_sentences, input_file, output_file, tokenizer)
+        process = Process(target=task)
+        process.start()
+        processes.append(process)
+        print(f"JamoTokenizer: {input_file.name} is processing...")
 
     # MecabTokenizer
     mecab_resources = [r for r in resources if r.startswith("mecab-")]
     for mecab_resource in mecab_resources:
-        output_file = f"dataset/modoo-translation/tokenized/ko_sentences_{mecab_resource}.txt"
         tokenizer = MeCabTokenizer(mecab_path=DICT_PATH, config_path=f"resources/{mecab_resource}/tok.json")
-        task = partial(tokenize_sentences, input_file, output_file, tokenizer)
-        print(f"mecab_resource: {mecab_resource}")
-        tasks.append(task)
+        for input_file in KO_CORPUSES:
+            output_file = f"dataset/modoo-translation/tokenized/{mecab_resource}/{input_file.name}"
+            task = partial(tokenize_sentences, input_file, output_file, tokenizer)
+            process = Process(target=task)
+            process.start()
+            processes.append(process)
+            print(f"MeCabTokenizer: {input_file.name} is processing with {mecab_resource}...")
 
     # SentencePieceTokenizer
     sp_resources = [r for r in resources if r.startswith("sp-")]
     for sp_resource in sp_resources:
-        output_file = f"dataset/modoo-translation/tokenized/ko_sentences_{sp_resource}.txt"
         tokenizer = SentencePieceTokenizer(model_path=f"resources/{sp_resource}/tok.model")
-        task = partial(tokenize_sentences, input_file, output_file, tokenizer)
-        print(f"sp_resource: {sp_resource}")
-        tasks.append(task)
+        for input_file in KO_CORPUSES:
+            output_file = f"dataset/modoo-translation/tokenized/{sp_resource}/{input_file.name}"
+            task = partial(tokenize_sentences, input_file, output_file, tokenizer)
+            process = Process(target=task)
+            process.start()
+            processes.append(process)
+            print(f"SentencePieceTokenizer: {input_file.name} is processing with {sp_resource}...")
 
     # en_sp tokenizer
-    input_file = "dataset/modoo-translation/en_sentences.txt"
     en_sp_resources = [r for r in resources if r.startswith("en_sp-")]
     for en_sp_resource in en_sp_resources:
-        output_file = f"dataset/modoo-translation/tokenized/en_sentences_{en_sp_resource}.txt"
         tokenizer = SentencePieceTokenizer(model_path=f"resources/{en_sp_resource}/tok.model")
-        task = partial(tokenize_sentences, input_file, output_file, tokenizer)
-        tasks.append(task)
-        print(f"en_sp_resource: {en_sp_resource}")
+        for input_file in EN_CORPUSES:
+            output_file = f"dataset/modoo-translation/tokenized/{en_sp_resource}/{input_file.name}"
+            task = partial(tokenize_sentences, input_file, output_file, tokenizer)
+            process = Process(target=task)
+            process.start()
+            processes.append(process)
+            print(f"SentencePieceTokenizer: {input_file.name} is processing with {en_sp_resource}...")
 
     # MeCabSentencePieceTokenizer
-    input_file = "dataset/modoo-translation/ko_sentences.txt"
     mecab_sp_resources = [r for r in resources if r.startswith("mecab_sp-")]
     for mecab_sp_resource in mecab_sp_resources:
-        output_file = f"dataset/modoo-translation/tokenized/ko_sentences_{mecab_sp_resource}.txt"
         tokenizer = MeCabSentencePieceTokenizer(
             mecab=MeCabTokenizer(mecab_path=DICT_PATH, config_path="resources/mecab-16k/tok.json"),
             sp=SentencePieceTokenizer(model_path=f"resources/{mecab_sp_resource}/tok.model"),
         )
+        for input_file in KO_CORPUSES:
+            output_file = f"dataset/modoo-translation/tokenized/{mecab_sp_resource}/{input_file.name}"
+            task = partial(tokenize_sentences, input_file, output_file, tokenizer)
+            process = Process(target=task)
+            process.start()
+            processes.append(process)
+            print(f"MeCabSentencePieceTokenizer: {input_file.name} is processing with {mecab_sp_resource}...")
+
+    # special group: serial run
+    print("=== Special group: serial run ===")
+    # WordTokenizer
+    print("WordTokenizer")
+    tokenizer = WordTokenizer()
+    for input_file in KO_CORPUSES:
+        output_file = f"dataset/modoo-translation/tokenized/word-64k/{input_file.name}"
         task = partial(tokenize_sentences, input_file, output_file, tokenizer)
-        print(f"mecab_sp_resource: {mecab_sp_resource}")
-        tasks.append(task)
-
-    assert len(tasks) == len(resources)
-
-    print("=" * 30)
-    print(f"Total {len(tasks)} tasks")
-    print("=" * 30)
-    # run all tasks
-    for task in tasks:
         task(pbar=True)
 
-    print("Done!")
+    print("=== Waiting for all processes to finish ===")
+    for process in tqdm(processes):
+        process.join()
+    print("=== All processes finished ===")
--- koen-preprocess-routine.sh
+++ koen-preprocess-routine.sh
@@ -4,15 +4,30 @@ workers=4
 srclang=ko
 destlang=en
 
-datadir=dataset/modoo-translation/processed
 resourcedir=resources
 
 function submit {
     source_dict=$1
     target_dict=$2
     job_name="${source_dict}-${target_dict}"
-    dest=dataset/modoo-translation/$job_name/preprocessed/$srclang-$destlang
+    datadir=$PWD/dataset/modoo-translation/$job_name
+    srclang_srcdir=$PWD/dataset/modoo-translation/tokenized/$source_dict
+    destlang_srcdir=$PWD/dataset/modoo-translation/tokenized/$target_dict
+    dest=$PWD/dataset/modoo-translation/$job_name/preprocessed/$srclang-$destlang
+
     rm -rf $dest
+    rm -rf $datadir
+    mkdir -p $datadir
+    mkdir -p $dest
+
+    ln -s $srclang_srcdir/train.$srclang $datadir/train.$srclang
+    ln -s $srclang_srcdir/validation.$srclang $datadir/validation.$srclang
+    ln -s $srclang_srcdir/test.$srclang $datadir/test.$srclang
+    ln -s $destlang_srcdir/train.$destlang $datadir/train.$destlang
+    ln -s $destlang_srcdir/validation.$destlang $datadir/validation.$destlang
+    ln -s $destlang_srcdir/test.$destlang $datadir/test.$destlang
+
+    echo "Submitted $job_name"
     fairseq-preprocess \
     --source-lang $srclang \
     --target-lang $destlang \
@@ -26,12 +41,35 @@ function submit {
 }
 
 submit char-2k en_sp-32k
-submit char-2k en_sp-64k
+submit jamo-200 en_sp-32k
+submit word-64k en_sp-32k
+submit mecab_sp-4k en_sp-32k
+submit mecab_sp-8k en_sp-32k
+submit mecab_sp-16k en_sp-32k
+submit mecab_sp-32k en_sp-32k
+submit mecab_sp-64k en_sp-32k
+submit mecab-8k en_sp-32k
+submit mecab-16k en_sp-32k
+submit mecab-32k en_sp-32k
+submit mecab-64k en_sp-32k
 submit sp-4k en_sp-32k
 submit sp-8k en_sp-32k
 submit sp-16k en_sp-32k
 submit sp-32k en_sp-32k
 submit sp-64k en_sp-32k
+
+submit char-2k en_sp-64k
+submit jamo-200 en_sp-64k
+submit word-64k en_sp-64k
+submit mecab_sp-4k en_sp-64k
+submit mecab_sp-8k en_sp-64k
+submit mecab_sp-16k en_sp-64k
+submit mecab_sp-32k en_sp-64k
+submit mecab_sp-64k en_sp-64k
+submit mecab-8k en_sp-64k
+submit mecab-16k en_sp-64k
+submit mecab-32k en_sp-64k
+submit mecab-64k en_sp-64k
 submit sp-4k en_sp-64k
 submit sp-8k en_sp-64k
 submit sp-16k en_sp-64k
